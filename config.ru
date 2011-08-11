6ea8f0cd2488c24554c0b9b8146858927b8542c0
Will Barrett <william.barrett@tallgreentree.com>
2011-08-11 16:16:59 -0500

    Initial import into SC

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './onepage'
+run Sinatra::Application
---
+++ onepage.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'erb'
+require 'sass'
+
+get '/' do
+  @title = 'Will Barrett'
+  erb :home
+end
+
+get '/stylesheet.css' do
+  sass :stylesheet
+end
